aba8ca9b02a4136786b045c98d064115a9992603
davidadel95 <davidadel95@outlook.com>
2021-02-03 18:35:53 +0200

    firestore pod

--- MTM/Features/Home/ViewController/HomeVC.swift
+++ MTM/Features/Home/ViewController/HomeVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class HomeVC: UIViewController {
 
@@ -13,6 +14,22 @@ class HomeVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let db = Firestore.firestore()
+        // Add a new document with a generated ID
+        var ref: DocumentReference? = nil
+        ref = db.collection("users").addDocument(data: [
+            "first": "Ada",
+            "last": "Lovelace",
+            "born": 1815
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+
     }
 
 
--- Podfile
+++ Podfile
@@ -7,14 +7,14 @@ target 'MTM' do
 
   # Pods for MTM
   
-#  pod 'Firebase/Firestore'
-
-  # Optionally, include the Swift extensions if you're using Swift.
-#  pod 'FirebaseFirestoreSwift'
-  
   # add the Firebase pod for Google Analytics
   pod 'Firebase/Analytics'
   # add pods for any other desired Firebase products
   # https://firebase.google.com/docs/ios/setup#available-pods
 
+
+  pod 'Firebase/Firestore'
+
+  # Optionally, include the Swift extensions if you're using Swift.
+  pod 'FirebaseFirestoreSwift'
 end
