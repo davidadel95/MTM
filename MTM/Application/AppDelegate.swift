06afe80f3d000da8c7c430a291878ddb30fe85c0
davidadel95 <davidadel95@outlook.com>
2021-02-03 18:14:46 +0200

    adding firebase pod

--- MTM/Application/AppDelegate.swift
+++ MTM/Application/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -23,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
         
+        FirebaseApp.configure()
         
         return true
     }
---
+++ Pods/Target Support Files/Pods-MTM/Pods-MTM-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MTM : NSObject
+@end
+@implementation PodsDummy_Pods_MTM
+@end
