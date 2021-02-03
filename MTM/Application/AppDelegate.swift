93617311da3ce4dcac51bd6b3ed2ab790617037b
davidadel95 <davidadel95@outlook.com>
2021-02-03 17:45:58 +0200

    init project

---
+++ MTM/Application/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  MTM
+//
+//  Created by David Adel on 03/02/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let mainController = HomeVC() as UIViewController
+        let navigationController = UINavigationController(rootViewController: mainController)
+        navigationController.navigationBar.isTranslucent = false
+        navigationController.navigationBar.isHidden = true
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+        
+        
+        return true
+    }
+
+}
+
